cbc9eeb877e7d98235129964a1378b3d8a17b3ff
pedrimbh <joaopmesquita@hotmail.com>
2022-10-31 09:56:28 -0300

    criacao de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        String nome = "Joao";
+        double salarioMinimo = 2500;
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 9876543;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double numero = 2132515;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        numero = 10;
+
+        System.out.println(((int)numero));
+
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
